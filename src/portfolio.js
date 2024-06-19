b2137ed7c8644265d0051fc0a71d09a0323c3166
Darnel Castor <darnelcastor2@gmail.com>
2024-06-19 11:09:04 -0400

    update portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -380,7 +380,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
+  number: "+1404-771-7591",
   email_address: "darnelcastor2@gmail.com"
 };
 
